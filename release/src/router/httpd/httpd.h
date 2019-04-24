odkrys/asuswrt-merlin.ng

httpd: webui: implement dropdown presets for DoT
The preset files are stored in /rom, so they can easily be
maintained without requiring modifying the webui content,
or even replaced by an end-user.

release/src/router/httpd/httpd.h
@@ -377,6 +377,9 @@ extern char* reverse_str( char *str );
 #ifdef RTCONFIG_AMAS
 extern int check_AiMesh_whitelist(char *page);
 #endif
+#ifdef RTCONFIG_DNSPRIVACY
+extern int ej_get_dot_presets(int eid, webs_t wp, int argc, char_t **argv);
+#endif
 
 /* web-*.c */
 extern int ej_wl_status(int eid, webs_t wp, int argc, char_t **argv, int unit);

gzenux/asuswrt-rtn18u

release/src/router/httpd/httpd.h
@@ -383,6 +383,9 @@ extern char* reverse_str( char *str );
 #ifdef RTCONFIG_AMAS
 extern int check_AiMesh_whitelist(char *page);
 #endif
+#ifdef RTCONFIG_DNSPRIVACY
+extern int ej_get_dnsprivacy_presets(int eid, webs_t wp, int argc, char_t **argv);
+#endif
 
 /* web-*.c */
 extern int ej_wl_status(int eid, webs_t wp, int argc, char_t **argv, int unit);