46d0b7048a215ac1e25bf5e89b0221ad89d576b2
YiJun Chen <gzenux@gmail.com>
2020-11-10 19:13:31 +0800

    RT-N18U: add missing function for LED stealth mode feature

--- release/src/router/rc/rtn18u.c
+++ release/src/router/rc/rtn18u.c
@@ -9,3 +9,24 @@ int LanWanLedCtrl(void)
 		return led_control(LED_LAN, LED_OFF);
 #endif
 }
+
+#if defined(RTCONFIG_LED_BTN) || defined(RTCONFIG_WPS_ALLLED_BTN) || defined(RTCONFIG_TURBO_BTN)
+void setAllLedNormal(void)
+{
+	led_control(LED_POWER, LED_ON);
+
+	/* Wireless LED */
+	if (nvram_match("wl0_radio", "1")) {
+		led_control(LED_2G, LED_ON);
+	}
+
+	/* LAN LED */
+	LanWanLedCtrl();
+
+	/* WAN LED */
+	kill_pidfile_s("/var/run/wanduck.pid", SIGUSR2);
+
+	/* USB LED */
+	kill_pidfile_s("/var/run/usbled.pid", SIGTSTP);
+}
+#endif
